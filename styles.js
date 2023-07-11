5e73fe045d9b1a7e4f70407e3ddbf9efb4d338e5
ibarraaa <114472374+ibarraaa@users.noreply.github.com>
2023-07-11 16:36:00 +0800

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,49 @@
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f2f2f2;
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  max-width: 600px;
+  margin: 40px auto;
+  padding: 20px;
+  background-color: #fff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  text-align: center;
+}
+
+h1 {
+  margin-bottom: 20px;
+}
+
+#question-container {
+  margin-bottom: 20px;
+  font-size: 20px;
+}
+
+#answer-buttons button {
+  display: block;
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10px;
+  background-color: #4caf50;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+}
+
+#next-button {
+  display: none;
+  padding: 10px 20px;
+  background-color: #4caf50;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+}
+
+#score-container {
+  margin-top: 20px;
+  font-weight: bold;
+}
